666dc0ea756163a4991e4756d7a993501e6a5853
CodeNinja04 <hemendrasharma2019@gmail.com>
2022-03-05 11:47:29 +0530

    some changes

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-
-
+import HomeImg from "./Home1.svg"
+import "./Home.css";
 
 const Home = () => {
-    return (<div className="App">
+    return (
+    <div className="App">
         <h1 className="text-3xl font-bold underline">
             Home
         </h1>
@@ -11,6 +12,8 @@ const Home = () => {
         <button className="bg-sky-600 hover:bg-sky-700">
             Save changes
         </button>
+
+        <img src={HomeImg} />
     </div>);
 }
 
